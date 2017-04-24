2f12f836bc48d3be4513bc8b487071b3c3558cbf
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2017-04-24 09:25:18 +0200

    took me far too long to get this working

---
+++ git-current-ref.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/michcioperz/raven_util"
+	"os"
+	"log"
+	"fmt"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "git-current-ref"
+	app.Usage = "extract current commit"
+	pwd, err := os.Getwd()
+  if err != nil {
+		pwd = "."
+	}
+	app.Flags = []cli.Flag {
+		cli.BoolFlag{
+			Name: "verbose",
+		},
+		cli.StringFlag{
+			Name: "path",
+			Value: pwd,
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		var l *log.Logger
+		if c.Bool("verbose") {
+			l = log.New(os.Stderr, "researching: ", log.LstdFlags)
+		} else {
+			l = nil
+		}
+		fmt.Println(raven_util.ExtractCurrentRelease(c.String("path"), l))
+		return nil
+	}
+	app.Run(os.Args)
+}
